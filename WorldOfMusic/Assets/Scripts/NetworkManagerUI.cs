218802f05012f4ab99ae63436832a8d7d48cfb09
Crims0nnn <m.brahim@ludus-academie.com>
2022-12-15 17:06:46 +0100

    Ajout de deux boutons permettant le choix des skins

--- WorldOfMusic/Assets/Scripts/NetworkManagerUI.cs
+++ WorldOfMusic/Assets/Scripts/NetworkManagerUI.cs
@@ -9,6 +9,10 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] private Button ServerBut;
     [SerializeField] private Button HostBut;
     [SerializeField] private Button ClientBut;
+    [SerializeField] private Button Color1;
+    [SerializeField] private Button Color2;
+    public int color = 0;
+    public int TailleTableau = 3;
 
     private void Awake(){
     ServerBut.onClick.AddListener(() => {
@@ -20,7 +24,27 @@ public class NetworkManagerUI : MonoBehaviour
     ClientBut.onClick.AddListener(() => {
         NetworkManager.Singleton.StartClient();
     });
-    } 
+    Color1.onClick.AddListener(() => {
+        color += 1;
+        if(color > TailleTableau){
+        color = 0;
+         }
+        if(color < 0){
+        color = TailleTableau;
+        Debug.Log(color);
+        }
+    });
+    Color2.onClick.AddListener(() => {
+        color -= 1;
+        if(color > TailleTableau){
+        color = 0;
+        }
+        if(color < 0){
+            color = TailleTableau;
+            Debug.Log(color);
+        }
+    });
+}
 }
 
 
