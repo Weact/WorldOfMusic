c51c5528d9a8dc10ae845a6cecb666b27dc8a3a4
Woakk <r.aguilar@ludus-academie.com>
2022-12-15 16:29:27 +0100

    Ajout d'un menu principal et de l'interaction avec ce dernier, réorganisation des ressources

--- WorldOfMusic/Assets/Resources/FirstPersonTemplate/Scripts/Interactions/SelectSkin.cs
+++ WorldOfMusic/Assets/Resources/FirstPersonTemplate/Scripts/Interactions/SelectSkin.cs
@@ -9,6 +9,7 @@ public class SelectSkin : MonoBehaviour
     public CanvasGroup menu;
 	public bool Affiched = false;
 	public bool ToucheL = false;
+	public int User = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -17,26 +18,27 @@ public class SelectSkin : MonoBehaviour
         AfficheMenu();
     }
     
-        void AfficheMenu()
+        public void AfficheMenu()
         {
-			Debug.Log("Touche M pressée - Menu affiché");
+			Debug.Log("Menu affiché");
             menu.alpha = 1;
             menu.interactable = true;
             menu.blocksRaycasts = true;
 			Affiched = true;
 			ToucheL = false;
 			//On bloque les touches de déplacement du joueur
-			
+			Cursor.lockState = CursorLockMode.None;
         }
    
-	     void CacheMenu()
+	     public void CacheMenu()
     	{       
-            Debug.Log("Touche M pressée - Menu caché");
+            Debug.Log("Menu caché");
             menu.alpha = 0;
             menu.interactable = false;
             menu.blocksRaycasts = false;
         	Affiched = false;
 			ToucheL = false;
+			Cursor.lockState = CursorLockMode.Locked;
     	}
 	
     
@@ -61,6 +63,33 @@ public class SelectSkin : MonoBehaviour
 	        ToucheL = true;
 		}
     }
+    
+        public void Exit()
+        {
+            Application.Quit();
+            Debug.Log("L'application est fermée");
+        }
+    
+        public void Artist()
+        {
+            User = 1;
+            Debug.Log("Artiste");
+        }
+        
+        public void Fan()
+        {
+            User = 2;
+            Debug.Log("Fan");
+        }
+        
+        public void Connect()
+        {
+            if(User != 0)
+            {
+                Debug.Log("Ptdr t co");
+                CacheMenu();
+            }
+        }
 
     
 
