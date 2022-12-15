efa23208457a92bfa9f59d727bf1cab9c4557238
Irthir <romain.schlotter@gmail.com>
2022-12-15 17:56:31 +0100

    LE SON MARCHE git add .!

---
+++ WorldOfMusic/Assets/Scripts/LocalPlayerManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VivoxUnity;
+
+public class LocalPlayerManager : MonoBehaviour
+{
+    [SerializeField]
+    private VivoxVoiceManager voiceManager;
+
+    private bool bChannel = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (voiceManager.LoginState==LoginState.LoggedOut)
+            ConnectVoice();
+        
+        if (voiceManager.LoginState==LoginState.LoggedIn && bChannel)
+            ConnectChannel();
+        
+    }
+
+    void ConnectVoice()
+    {
+        voiceManager.Login("Test");
+    }
+
+    void ConnectChannel()
+    {
+        bChannel = false;
+        voiceManager.JoinChannel("Test", ChannelType.NonPositional, VivoxVoiceManager.ChatCapability.AudioOnly);
+        //voiceManager.OnSpeechDetectedEvent += OnSpeechDetected;
+    }
+    
+    void OnSpeechDetected(string participantUri, ChannelId id, bool isSpeaking)
+    {
+        Debug.Log("OnSpeechDetected: " + participantUri + " isSpeaking: " + isSpeaking);
+    }
+}
