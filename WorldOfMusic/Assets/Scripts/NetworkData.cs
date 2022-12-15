b0d286b681b64647574493e2c61ea7b596ae9614
Irthir <romain.schlotter@gmail.com>
2022-12-15 11:24:39 +0100

    Tests sur les build serveur concluant, cependant besoin d'un personnage complet pour tester le voyage entre les scènes

--- WorldOfMusic/Assets/Scripts/NetworkData.cs
+++ WorldOfMusic/Assets/Scripts/NetworkData.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 
 public class NetworkData : NetworkBehaviour
 {
+    void NCalcul_OnValueChanged(int ancien, int nouveau)
+    {
+        Debug.Log("Prout");
+    }
+    
     public NetworkVariable<int> nCalcul;
     // Start is called before the first frame update
     void Start()
     {
         nCalcul = new NetworkVariable<int>(0);
+        nCalcul.OnValueChanged += NCalcul_OnValueChanged;
     }
-
+    
     public void Incremente()
     {
         nCalcul.Value += 1;
@@ -20,6 +26,6 @@ public class NetworkData : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(nCalcul.Value); //GameObject.FindObjectsOfType<NetworkPlayerLocal>().Length);
+        //Debug.Log(nCalcul.Value); //GameObject.FindObjectsOfType<NetworkPlayerLocal>().Length);
     }
 }
