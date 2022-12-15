9870b99ca75a1a96de1d983dad39e36c16b0560d
creed92 <abdelaziz.kh@ludus-academie.com>
2022-12-15 17:19:26 +0100

    update music list with script error

--- WorldOfMusic/Assets/Scripts/PlayListManager.cs
+++ WorldOfMusic/Assets/Scripts/PlayListManager.cs
@@ -7,44 +7,63 @@ using TMPro;
 
 public class PlayListManager : MonoBehaviour
 {
+
+
+    public AudioSource m_AudioSource;
+    
+
     [Serializable]
     public struct MusicList
     {
         public string nom;
         public Sprite image;
         public string description;
-        public AudioSource music;
+        public AudioClip music;
+        public int index;
 
     }
     [SerializeField] MusicList[] musiclist;
 
-
-
-
+    
+    
+    public AudioClip[] listMusique;
+    
     void Start()
     {
        
-
+       m_AudioSource= GetComponent<AudioSource>();
+      
 
         GameObject ButMusic = transform.GetChild(0).gameObject;
         GameObject g;
         
-        int N = musiclist.Length;
+    int N = musiclist.Length;
+
 
         for (int i = 0; i < N; i++)
         {
-           
-            g = Instantiate(ButMusic, transform);
-            g.SetActive(true);
+            musiclist[i].index = i ;
+            g = Instantiate(ButMusic, transform);    
             g.transform.GetChild(0).GetComponent<Image>().sprite = musiclist[i].image;
             g.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = musiclist[i].nom;
             g.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = musiclist[i].description;
-           
+            g.transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = musiclist[i].index.ToString();
 
-            Destroy(ButMusic);
+            listMusique[i]= musiclist[i].music;
+            g.GetComponent<Button>().onClick.AddListener(() => PlayMusic(i));
+            
+            
+
         }
+        Destroy(ButMusic);   
     }
 
- 
+    public void PlayMusic(int k)
+    {
+
+       
+         m_AudioSource.clip = listMusique[k];
+         m_AudioSource.Play();
+    }
 
 }
